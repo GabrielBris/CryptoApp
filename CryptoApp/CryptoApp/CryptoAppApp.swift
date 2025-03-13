ecf77df60e64c82ea8175c81c10afe52af763b31
Gabriel Briseño <gabriel_bris1315@outlook.com>
2025-03-13 15:39:16 -0600

    [ADD] Added Xcode project base

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Gabriel Alejandro Briseño Alvarez on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
